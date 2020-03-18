55ee7564469432288185e34145f4efa93a23d7d8
The Tran <tran82h@gmail.com>
2020-03-18 12:28:26 -0500

    need to change in more stuff

--- index.js
+++ index.js
@@ -10,4 +10,6 @@ require('./startup/validation')();
 require('./startup/prod')(app);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => winston.info(`Listening on port ${port}...`));
+const server = app.listen(port, () => winston.info(`Listening on port ${port}...`))
+
+module.exports = server;
--- startup/db.js
+++ startup/db.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 const winston = require('winston');
+const config = require('config');
 
 module.exports = function () {
+  const db = config.get('db');
   mongoose
-    .connect("mongodb://localhost/wedding", {
+    .connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
